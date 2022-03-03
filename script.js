071faf9374e19be11d7ef414369425833ce4314a
georgesarreas <george.sarreas@gmail.com>
2022-03-03 14:43:00 +0200

    logic behind game completed

--- script.js
+++ script.js
@@ -69,3 +69,70 @@ console.log(game());
 
 
 
+/* here on down is the code for the ui rock-paper-scissors*/
+
+const choices = document.querySelectorAll(".choices");
+const score  = document.getElementById('score');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const result = document.getElementById('result');
+const totalScore = {
+    player: 0,
+    computer: 0
+};
+
+
+//function that plays the round
+function playRound(e) {
+    restart.style.display = 'inline-block';
+    const playerSelection = e.target.id;
+    const computerSelection = computerPlay();
+    const winner= determineWinner(playerSelection,computerSelection);
+    console.log(playerSelection,computerSelection,winner);
+};
+
+//the computer's choice function
+const options = ['rock', 'paper', 'scissors'];
+
+function computerPlay(){
+    return options[Math.floor(Math.random()*3)];
+}
+
+//determine winner
+function determineWinner(playerSelection,computerSelection) {
+    if (computerSelection === 'rock' ){
+        if (playerSelection === 'rock'){
+            return 'draw'
+        }else if (playerSelection === 'paper'){
+            
+            return 'player'
+        }else if (playerSelection === 'scissors'){
+            
+            return 'computer';
+        }
+
+    } else if (computerSelection === 'paper'){
+        if (playerSelection === 'rock'){
+            
+            return 'computer';
+        }else if (playerSelection === 'paper'){
+            return 'draw';
+        }else if ( playerSelection === 'scissors' ) {
+            
+            return 'player';
+        }
+
+    }else if (computerSelection === 'scissors'){
+        if (playerSelection === 'rock') {
+            
+            return 'player';
+        }else if (playerSelection === 'paper'){
+            
+            return 'computer';
+        }else if (playerSelection === 'scissors'){
+            return ' draw';
+        }
+    }
+}
+//now for the event listener
+choices.forEach(choice => choice.addEventListener('click', playRound));
